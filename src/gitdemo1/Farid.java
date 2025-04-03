e2037d89c11f23cadc3fd65100f966690deb6c14
“FaridJavaDev” <farid.aini@yahoo.com>
2025-04-03 08:41:29 -0500

    day2 commit2

--- src/gitdemo1/Farid.java
+++ src/gitdemo1/Farid.java
@@ -5,6 +5,9 @@ public class Farid {
 	public static void main(String[] args) {
 		System.out.println("Helo World!");
 		System.out.println("Day2");
+		System.out.println("Day2 commit2");
+		System.out.println("Day2 commit2");
+		System.out.println("Day2 commit2");
 	}
 
 }
